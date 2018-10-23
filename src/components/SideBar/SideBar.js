875b25ac9f3822ca42459fb3c2eb7340b47005b7
Alex_Sandok <alex.pro.sandok@gmail.com>
2018-10-23 13:57:46 +0300

    adding sidebar component

---
+++ src/components/SideBar/SideBar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+// styles
+import './SideBar.scss'
+
+const SideBar = props => {
+  return (
+    <div className="sidebar">
+      <p>Search</p>
+      <div>clients</div>
+    </div>
+  )
+}
+
+export default SideBar
--- src/containers/Main/Main.js
+++ src/containers/Main/Main.js
@@ -1,11 +1,12 @@
 // react
 import React, { Component } from 'react'
-import PropTypes from 'prop-types'
+// import PropTypes from 'prop-types'
 
 //redux
 import { connect } from 'react-redux'
 
 // components
+import SideBar from '../../components/SideBar/SideBar'
 
 // hoc's
 import Layout from '../../HOC/Layout/Layout'
@@ -20,19 +21,21 @@ class Main extends Component {
   render() {
     return (
       <Layout>
-        <p>Hello main page</p>
+        <div className="main-wrapper">
+          <SideBar />
+        </div>
       </Layout>
     )
   }
 }
 
 const mapStateToProps = state => ({
-  users: state.users
+  users: state.users,
 })
 
 const mapDispatchToProps = dispatch => () => dispatch(getUsers())
 
 export default connect(
   mapStateToProps,
-  mapDispatchToProps
+  mapDispatchToProps,
 )(Main)
