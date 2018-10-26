013fd3dc7b9e37e2cb62205ed8dfc2d12f0f0a0d
Alex_Sandok <alex.pro.sandok@gmail.com>
2018-10-26 10:17:30 +0300

    fixed resultselector

--- src/selectors/clientsSelector.js
+++ src/selectors/clientsSelector.js
@@ -4,6 +4,6 @@ export const getClientList = ({ clients }) => clients.items || {}
 export const getClientById = id =>
   createSelector(getClientList, clients => clients.find(i => i.id === id))
 
-export const getClientSearchResult = ({ clients }) => clients.result || ''
+export const getClientSearchResult = ({ clients }) => clients.result
 export const clientSearchResult = value =>
   createSelector(getClientSearchResult, clients => clients.result + value)
